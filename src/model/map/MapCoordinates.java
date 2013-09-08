4ae85ae5a7f9b6f824fc79976b38110d8e1141c1
Till Hülsemann <thgithub@gmail.com>
2013-09-08 09:52:15 +0200

    add class MapCoordinates
    add class that contains public integers for x, y and z to group
    coordinates and reduce the need to give the values in a specific order

---
+++ src/model/map/MapCoordinates.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package model.map;
+
+/**
+ * @author till
+ *
+ */
+public class MapCoordinates {
+	public int x = 0;
+	public int y = 0;
+	public int z = 0;
+	
+	public MapCoordinates(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
