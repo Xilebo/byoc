8c6d0e1d91256f7511c4070b478a5eda56866666
Till Hülsemann <thgithub@gmail.com>
2013-09-24 15:57:07 +0200

    clean code for MapCoordinates
    * add/correct comments
    * add toString
    * add hashCode
    * add equals

--- src/model/map/MapCoordinates.java
+++ src/model/map/MapCoordinates.java
@@ -1,10 +1,9 @@
 package model.map;
 
 /**
- * @author till
- * 
  * contains public integers for x, y and z
  *
+ * @author till
  */
 public class MapCoordinates {
 	public int x;
@@ -12,9 +11,8 @@ public class MapCoordinates {
 	public int z;
 	
 	/**
-	 * @author till
-	 *
 	 * initiates all coordinates with 0
+	 * 
 	 */
 	public MapCoordinates() {
 		this.x = 0;
@@ -22,9 +20,49 @@ public class MapCoordinates {
 		this.z = 0;
 	}
 	
+	/**
+	 * initiates coordinates with given values
+	 * 
+	 * @param x - the x coordinate
+	 * @param y - the y coordinate
+	 * @param z - the z coordinate
+	 */
 	public MapCoordinates(int x, int y, int z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+	}
+
+	@Override
+	public String toString() {
+		return "[x=" + x + ", y=" + y + ", z=" + z + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		result = prime * result + z;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MapCoordinates other = (MapCoordinates) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		if (z != other.z)
+			return false;
+		return true;
 	}
 }
